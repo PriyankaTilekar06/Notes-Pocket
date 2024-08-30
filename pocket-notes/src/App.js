a1a1294f657e702f243830db3ff6a3170ec7614d
Priyanka <priyankatilekar003@gmail.com>
2024-08-30 14:06:55 +0530

    createpopup commit

--- pocket-notes/src/App.js
+++ pocket-notes/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import CreaatePopup from './components/CreaatePopup';
+import CreaatePopup from './components/CreatePopupDesktop/CreatePopup';
 
 function App() {
   return (

79d5c0a9dbd1d631ceb624dfb32ee1c27dc57e01
Priyanka <priyankatilekar003@gmail.com>
2024-08-30 13:56:40 +0530

    popup

---
+++ pocket-notes/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import CreaatePopup from './components/CreaatePopup';
+
+function App() {
+  return (
+    <div>
+      Hello world
+      <CreaatePopup />
+    </div>
+  );
+}
+
+export default App;
